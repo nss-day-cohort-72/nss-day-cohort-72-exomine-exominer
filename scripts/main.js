99bd9036b3b28bbcda59df867152769bc720c5a1
Wilmer Morales <moralesw.1981@gmail.com>
2024-08-27 09:19:39 -0500

    main html sketchy

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,42 @@
 import {GovernorsList} from "./GovernorsList.js"
 
 
-const container = document.querySelector("#container")
+// const container = document.querySelector("#container")
 
 // const render = async () => {
-//     const jeanOwnerShipHTML = OwnJeansChoices()
-//     const locationsHTML = await LocationTypeChoices()
-//     const buttonHTML = await SaveSubmission()
-//     const submissionListHTML = await SubmissionList()
-//     container.innerHTML = `${jeanOwnerShipHTML} 
-//     ${locationsHTML}
-//     ${buttonHTML}
-//     ${submissionListHTML}
-    
+//     const metalOptionsHTML = await MetalOptions()
+
+//     const composedHTML = `
+//         <h1>Kneel Diamonds</h1>
+
+//         <article class="choices">
+//             <section class="choices__metals options">
+//                 <h2>Metals</h2>
+//                 ${metalOptionsHTML}
+//             </section>
+
+//             <section class="choices__sizes options">
+//                 <h2>Sizes</h2>
+//             </section>
+
+//             <section class="choices__styles options">
+//                 <h2>Styles</h2>
+//             </section>
+//         </article>
+
+//         <article class="order">
+
+//         </article>
+
+//         <article class="customOrders">
+//             <h2>Custom Jewelry Orders</h2>
+
+//         </article>
 //     `
-//  }
+
+//     container.innerHTML = composedHTML
+// }
+// document.addEventListener("nameYouChose", event => {
+//     console.log("State of data has changed. Regenerating HTML...")
+//     render()
+// })
