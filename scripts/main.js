92e3388a7473dbea2bfcc8b98d1b74935a5dc722
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-27 09:14:04 -0500

    Add purchase button

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
-import {GovernorsList} from "./GovernorsList.js"
+import { GovernorsList } from './GovernorsList.js';
 
-
-const container = document.querySelector("#container")
+const container = document.querySelector('#container');
 
 // const render = async () => {
 //     const jeanOwnerShipHTML = OwnJeansChoices()
 //     const locationsHTML = await LocationTypeChoices()
 //     const buttonHTML = await SaveSubmission()
 //     const submissionListHTML = await SubmissionList()
-//     container.innerHTML = `${jeanOwnerShipHTML} 
+//     container.innerHTML = `${jeanOwnerShipHTML}
 //     ${locationsHTML}
 //     ${buttonHTML}
 //     ${submissionListHTML}
-    
+
 //     `
 //  }
