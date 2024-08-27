ed85969f4be058e34c9552753245512730bc0dc9
Robert Mack <robertmack123123@gmail.com>
2024-08-27 10:28:32 -0500

    Added colony inventory display

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,16 @@
-import { Facilities } from "./Facility.js"
-import {Governors} from "./GovernorsList.js"
-const render = async () => {
-    const governorsHTML = await Governors()
-    const facilityHTML = await Facilities()
+import { Facilities } from "./Facility.js";
+import { Governors } from "./GovernorsList.js";
 
-    //     const locationsHTML = await LocationTypeChoices()
-//     const buttonHTML = await SaveSubmission()
-//     const submissionListHTML = await SubmissionList()
-//     container.innerHTML = `${jeanOwnerShipHTML} 
-//     ${locationsHTML}
-//     ${buttonHTML}
-//     ${submissionListHTML}
-    
-//     `
+const render = async () => {
+    const governorsHTML = await Governors();
+    const facilityHTML = await Facilities();
+
     const composedHTML = `
     ${governorsHTML}
     ${facilityHTML}
-    `
-    const container = document.querySelector("#container")
-    container.innerHTML = composedHTML
- }
- render()
+    `;
+    const container = document.querySelector("#container");
+    container.innerHTML = composedHTML;
+};
+
+render();
