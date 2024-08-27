42438f374dd772ab847f861ed0749abf733e4d77
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-27 15:51:54 -0500

    Removed clicked event from purchase button and changed to change event for facility.js

--- scripts/PurchaseBtn.js
+++ scripts/PurchaseBtn.js
@@ -9,17 +9,3 @@ export const purchaseBtn = () => {
     </div>
     `;
 };
-
-document.addEventListener('click', async (e) => {
-  const para = document.querySelector('.cartItems');
-  const copyOfTransientState = transientStateCopy();
-  const transientMineralId = copyOfTransientState.mineralId;
-
-  const response = await fetch(' http://localhost:8088/minerals');
-  const minerals = await response.json();
-  const matchedMineral = minerals.find(
-    (mineral) => mineral.id === transientMineralId
-  );
-
-  para.innerHTML = `1 ton of ${matchedMineral.name}`;
-});
