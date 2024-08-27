b4a3199bcf3de88c61822bc7247fdc8b8611f52a
Sickle41 <sickle41@gmail.com>
2024-08-27 09:38:06 -0500

    facility1

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -3,13 +3,13 @@ export const FacilityOptions = async () => {
     const response = await fetch("http://localhost:8088/facilities");
     const facilities = await response.json();
 
-    let facilityOptionsHTML = "<label for='facilityDropdown'>Choose a facility:</label><select id='facilityDropdown'>";
-
-    const optionStringArray = facilities.map(
-        (facility) => {
-            return `<option value='${facility.id}'>${facility.name}</option>`;
-        }
-    );
+    let facilityOptionsHTML = "<span>Choose a facility:</span> <label for='facilityDropdown'>Choose a facility:</label><select id='facilityDropdown'>";
+    
+    const optionStringArray = facilities
+    .filter(facility => facility.active)
+    .map(facility => {
+        return `<option value='${facility.id}'>${facility.name}</option>`;
+    });
 
     facilityOptionsHTML += optionStringArray.join("") + "</select>";
 
