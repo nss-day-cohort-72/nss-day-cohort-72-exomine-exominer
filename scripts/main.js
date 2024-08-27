ce48776c639efb9d7394b0de48121c91eb16707d
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-27 11:01:29 -0500

    Add logic for minerals but will refactor for facilities

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,23 @@
-import {Governors} from "./GovernorsList.js"
-
-
-
+import { Governors } from './GovernorsList.js';
+import { Minerals } from './MineralsList.js';
 
 const render = async () => {
-    const governorsHTML = await Governors()
-    //     const locationsHTML = await LocationTypeChoices()
-//     const buttonHTML = await SaveSubmission()
-//     const submissionListHTML = await SubmissionList()
-//     container.innerHTML = `${jeanOwnerShipHTML}
-//     ${locationsHTML}
-//     ${buttonHTML}
-//     ${submissionListHTML}
+  const governorsHTML = await Governors();
+  //     const locationsHTML = await LocationTypeChoices()
+  //     const buttonHTML = await SaveSubmission()
+  //     const submissionListHTML = await SubmissionList()
+  //     container.innerHTML = `${jeanOwnerShipHTML}
+  //     ${locationsHTML}
+  //     ${buttonHTML}
+  //     ${submissionListHTML}
 
-//     `
-    const composedHTML = `
+  //     `
+  const composedHTML = `
     ${governorsHTML}
-    `
-    const container = document.querySelector("#container")
-    container.innerHTML = composedHTML
- }
- render()
+    `;
+  const container = document.querySelector('#container');
+  container.innerHTML = composedHTML;
+};
+render();
+
+Minerals();
