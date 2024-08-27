8f06e352bc3b03eef2cb9f106dc4eaefcba0633e
Wilmer Morales <moralesw.1981@gmail.com>
2024-08-27 09:46:01 -0500

    Facility.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,9 @@
+import { Facilities } from "./Facility.js"
 import {Governors} from "./GovernorsList.js"
-
-
-
-// const container = document.querySelector("#container")
-
-// const render = async () => {
-//     const metalOptionsHTML = await MetalOptions()
-
-//     const composedHTML = `
-//         <h1>Kneel Diamonds</h1>
-
-//         <article class="choices">
-//             <section class="choices__metals options">
-//                 <h2>Metals</h2>
-//                 ${metalOptionsHTML}
-//             </section>
-
-//             <section class="choices__sizes options">
-//                 <h2>Sizes</h2>
-//             </section>
-
-//             <section class="choices__styles options">
-//                 <h2>Styles</h2>
-//             </section>
-//         </article>
-
-//         <article class="order">
-
-//         </article>
-
-//         <article class="customOrders">
-//             <h2>Custom Jewelry Orders</h2>
-
-//         </article>
-//     `
-
-//     container.innerHTML = composedHTML
-// }
-// document.addEventListener("nameYouChose", event => {
-//     console.log("State of data has changed. Regenerating HTML...")
-//     render()
-// })
 const render = async () => {
     const governorsHTML = await Governors()
+    const facilityHTML = await Facilities()
+
     //     const locationsHTML = await LocationTypeChoices()
 //     const buttonHTML = await SaveSubmission()
 //     const submissionListHTML = await SubmissionList()
@@ -54,6 +15,7 @@ const render = async () => {
 //     `
     const composedHTML = `
     ${governorsHTML}
+    ${facilityHTML}
     `
     const container = document.querySelector("#container")
     container.innerHTML = composedHTML
